a9e3dd836002b31f2eafaf424982620df3f1593a
Attila Závoczki <atus.zavoczki@gmail.com>
2025-09-02 00:35:43 +0200

    doc: refined boarding introduction

--- src/components/Glossary.js
+++ src/components/Glossary.js
@@ -4,32 +4,54 @@ export const glossary = {
       <div>The Waveshare Solar Power Manager handles Li-ion charging, status LEDs, and step-down rails.</div>
       <img src="https://mechaxil.com/cdn/shop/files/IMG_20250815_154927.jpg?v=1755350653&width=1200"
            alt="Waveshare Solar Module"
-           style="max-width:100%;margin-top:0.5rem;" />
-    `,
+           style="max-width:100%;margin-top:0.5rem;" />`,
     link: "https://mechaxil.com/products/sensor-solar-manager-d-ws?variant=55265966195036"
   },
+
   "Raspberry Pi Pico": {
     content: `
       <div>A low-cost microcontroller board from Raspberry Pi, based on the RP2040 dual-core ARM Cortex-M0+ chip.</div>
       <img src="https://mechaxil.com/cdn/shop/files/IMG_20250815_155310.jpg?v=1755350246&width=1200"
            alt="Raspberry Pi Pico"
-           style="max-width:100%;margin-top:0.5rem;" />
-    `,
+           style="max-width:100%;margin-top:0.5rem;" />`,
     link: "https://mechaxil.com/products/mcu-rpi-pico?variant=55267271606620"
   },
+
   ESP32: {
     content: `<div>A low-cost, low-power system on a chip (SoC) with integrated Wi-Fi and dual-mode Bluetooth.</div>`,
   },
+
   "18650 Cell": {
     content: `<div>A rechargeable lithium-ion battery cell commonly used in portable electronics and electric vehicles.</div>`,
     link: "https://mechaxil.com/products/18650-cell"
   },
+
   "Li-ion Charging": {
     content: `<div>The process of charging lithium-ion batteries, which involves applying a controlled voltage and current to safely replenish the battery's energy.</div>`
     // no link needed
   },
+
   "Development board of choice": {
-    content: `<div>A printed circuit board (PCB) that includes a microcontroller or microprocessor, along with necessary components and interfaces for prototyping and development.</div>`,
+    content: `<div>A printed circuit board (PCB) that includes a microcontroller or microprocessor, along with necessary components and interfaces for prototyping and development.</div>
+          <img src="https://mechaxil.com/cdn/shop/files/IMG_20250815_155310.jpg?v=1755350246&width=1200"
+           alt="Raspberry Pi Pico"
+           style="max-width:100%;margin-top:0.5rem;" />`,
     link: "https://mechaxil.com/collections/microcontrollers"
   },
-  };
+
+  "Tooltips without links": {
+    content: `<div>Small pop-up boxes that provide additional information when hovering over or clicking on an element.</div> 
+    <img src="img/mechaxil_robot.png" style="max-width:100%;margin-top:.5rem;" alt="Mechaxil robot" >` 
+  },
+
+  "Tooltips with links": {
+    content: `<div>Small pop-up boxes that provide additional information when hovering over or clicking on an element.</div> 
+    <img src="img/mechaxil_robot.png" style="max-width:100%;margin-top:.5rem;" alt="Mechaxil robot" >` ,
+    link: "https://mechaxil.com"
+  },
+
+  "BOM": {
+    content: `<div>A Bill of Materials (BOM) is a comprehensive list of parts, items, assemblies, and other materials required to create a product.</div>`,
+  },
+
+  }
